17e00e0ecea140d6ce5767bb8e5d18ef20326f10
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-21 10:49:43 -0300

    SwitchMap

--- src/app/acoes/acoes.component.ts
+++ src/app/acoes/acoes.component.ts
@@ -1,4 +1,4 @@
-import { tap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 import { AcoesService } from './acoes.service';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
@@ -10,7 +10,11 @@ import { FormControl } from '@angular/forms';
 })
 export class AcoesComponent {
   acoesInput = new FormControl();
-  acoes$ = this.acoesInput.valueChanges.pipe(tap(console.log));
+  acoes$ = this.acoesInput.valueChanges.pipe(
+    tap(console.log),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
+    tap(console.log)
+  );
 
   constructor(private acoesService: AcoesService) {}
 }
